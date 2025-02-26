d31847ec50859a154a4314f91412571eaa91b097
Andrey <elanser2005@gmail.com>
2025-02-26 11:39:42 +0300

    Some fixes

--- create_bot.py
+++ create_bot.py
@@ -4,13 +4,11 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from decouple import config
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from db_handler import db_class
 from lang import Language
 
 translation = Language(config('LANGUAGE'), config('LANGUAGE_FILE'))
 database = db_class.DatabaseHandler(config('DATABASE'))
-# scheduler = AsyncIOScheduler(timezone='Europe/Moscow')
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
